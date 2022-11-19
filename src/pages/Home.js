7fce77fbf87c10aa4b0b015a710d2dad35f39970
Abigail Douglas <EburgAbby@aol.com>
2022-11-18 19:50:46 -0800

    updated img names

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -109,15 +109,15 @@ export default function Home() {
                     <div id="Contact_Me_Content">
                         <ul class="Contact_Me_Content">
                             <li class="Contact_Me_Content"><a href="https://github.com/AbbyDabbyDooDoesGitHub" target="_blank" rel="noreferrer">
-                                <img class="contact_icons" src={require("../assets/imgs/_icons/logo_github_icon_143196.png")} alt="GitHub Logo Icon"/>
+                                <img class="contact_icons" src={require("../assets/imgs/_icons/logo_github.png")} alt="GitHub Logo Icon"/>
                             </a></li>
 
                             <li class="Contact_Me_Content"><a href="https://www.linkedin.com/in/abigailcdouglas/" target="_blank" rel="noreferrer">
-                                <img class="contact_icons" src={require("../assets/imgs/_icons/logo_linkedin_icon_143191.png")} alt="LinkedIn Logo Icon"/>
+                                <img class="contact_icons" src={require("../assets/imgs/_icons/logo_linkedin.png")} alt="LinkedIn Logo Icon"/>
                             </a></li>
 
                             <li class="Contact_Me_Content"><a href="mailto: ItsAbigailDouglas@gmail.com">
-                                <img class="contact_icons" src={require("../assets/imgs/_icons/logo_gmail_envelope_letter_email_icon_143171.png")} alt="Gmail Logo Icon"/>
+                                <img class="contact_icons" src={require("../assets/imgs/_icons/logo_gmail.png")} alt="Gmail Logo Icon"/>
                             </a></li>
 
                         </ul>
