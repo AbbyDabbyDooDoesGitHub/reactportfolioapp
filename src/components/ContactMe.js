224795d2ac390c41406e14ebaab4ea8511409569
Abigail Douglas <EburgAbby@aol.com>
2022-11-20 02:06:54 -0800

    adding a bit of css and getting contact form looking nice

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -14,31 +14,38 @@ const ContactMeComp = () => {
             <div class="content">
                 <div id="Contact_Me_Content">
 
-                    <div class="row">
+                    <div class="row" id="contactForm-div">
                         <form class="col s12">
 
-                            <div class="row">
+                            <div class="contactForm-info-div row">
+                                <label for="contact_name">NAME</label>
                                 <div class="input-field col s12">
-                                <input placeholder="Your Name Here" id="contact_name" type="text" class="validate"/>
-                                <label for="contact_name">Name</label>
+                                    <input placeholder="John Doe" id="contact_name" type="text" class="validate"/>
                                 </div>
                             </div>
 
-                            <div class="row">
+                            <div class="contactForm-info-div row">
+                                <label for="contact_email">EMAIL</label>
                                 <div class="input-field col s12">
-                                <input id="contact_email" type="email" class="validate"/>
-                                <label for="contact_email">Email</label>
+                                    <input placeholder="jDoe@email.com" id="contact_email" type="email" class="validate"/>
                                 </div>
                             </div>
 
-                            <div class="row">
-                                <div class="input-field col s12">
-                                <input id="contact_message" type="text"/>
-                                <label for="contact_message">Message</label>
+                            <div class="contactForm-info-div row">
+                                <label for="contact_message">MESSAGE</label>
+                                <div  class="input-field col s12">
+                                    <input placeholder="Let's connect" id="contact_message" type="text"/>
                                 </div>
                             </div>
 
                         </form>
+
+                        <div id="contactForm-footerDiv">
+
+                            <a id="contact-submitBtn" href="/ContactMe">SUBMIT</a>
+
+                        </div>
+
                     </div>
         
                 </div>
